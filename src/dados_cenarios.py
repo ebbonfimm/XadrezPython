352daa9f5802392b6fbea2cf480d7a15fce2ad8c
ebbonfim <ebbonfim@sga.pucminas.br>
2026-05-04 20:53:09 -0300

    Adição do Cenário 4 para testes

--- src/dados_cenarios.py
+++ src/dados_cenarios.py
@@ -20,9 +20,20 @@ mapeamento_cenario_2 = {
 
 
 mapeamento_cenario_3 = {
-    'PB0': [3, 5],
-    'PB1': [3, 4],
+    'PB0': [3, 6],
+    'PB1': [3, 5],
 
     'PP0': [3, 3]
+
+}
+
+# Mapeamento 4: Testando movimento do Peão quando tem uma peça na posição X 
+# maior ou menor, mas não necessariamento na frente.
+mapeamento_cenario_4 = {
+    'PP0': [2, 1],
+    'PP1': [3, 1],
+
+    'CB0': [3, 2],
+    'PB0': [2, 6]
 
 }
